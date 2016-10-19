b06f78e0c8c4339e82341a214ad6835411457bf4
ytsr1988 <dragonpw1988@gmail.com>
2016-10-19 00:32:14 -0600

    first change

---
+++ HelloWorld/src/HelloWorldJ.java
@@ -0,0 +1,11 @@
+
+public class HelloWorldJ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("hello world 2");
+
+	}
+
+}
